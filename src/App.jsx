a0b5ecac8a162fe43196b31cbd09c63526d43e0f
HenriqueHiideki <henrique.hiideki@gmail.com>
2026-08-01 21:00:01 -0300

    Estrutura do frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return(
+    <main>
+      <header>
+
+      </header>
+
+      <section>
+        
+      </section>
+    </main>
+  )
+}
+
+export default App
