5762b6f6b92dabeadca135129df3ee00a25da0ac
HenriqueHiideki <henrique.hiideki@gmail.com>
2026-08-06 23:52:58 -0300

    Ajusta tamanho dos icones

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,32 @@ function App() {
         <section>
           <SideBar />
         </section>
-        <section>
+        <section className="content">
           <DescriptionTitle>Explore Tendências</DescriptionTitle>
           <div className="container-cards">
             <FilterTabs>Todos</FilterTabs>
             <FilterTabs>Ativos</FilterTabs>
             <FilterTabs>Encerrados</FilterTabs>
             <FilterTabs>Minhas enquetes</FilterTabs>
-            <FilterTabs><img src="/icon-filter.png" alt="icone de filtro" className='button-img' />
-              <img src="/icon-filter-white.png" alt="icone de filtro branco" className="button-img-hover"/> Filtro</FilterTabs>
+            <FilterTabs>
+              <img
+                src="/icon-filter.png"
+                alt="icone de filtro"
+                className="button-img"
+              />
+              <img
+                src="/icon-filter-white.png"
+                alt="icone de filtro branco"
+                className="button-img-hover"
+              />{" "}
+              Filtro
+            </FilterTabs>
           </div>
 
-          <div>
-            <Cards></Cards>
+          <div className="container-cards">
+            <Cards>Qual linguagem devemos usar?</Cards>
+            <Cards>Qual tecnologia voce tem experiencia?</Cards>
+            <Cards>Qual linguagem é mais eficaz?</Cards>
           </div>
         </section>
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,13 +2,24 @@ import { ProgressBar } from '../ProgressBar/PorgressBar'
 import './cards-style.css'
 import { CardText } from './CardsText'
 
-export function Cards(){
+export function Cards(props){
     return(
         <div className='cards'>
-            <CardText>Qual linguagem usar no projeto?</CardText>
+            <img src="icon-dot.png" className='card-img' />
+            <CardText>{props.children}</CardText>
             <ProgressBar label="Java" percentage={58} />
             <ProgressBar label="C#" percentage={32} />
             <hr className='card-line' />
+
+            <div className='footer-item'>
+                <img src="icon-votes.png" alt="Votos" className='card-img' />
+                <span>1.240 votos</span>
+            </div>
+
+            <div className='footer-item time'>
+                <img src="icon-watch.png" alt="Tempo" className='card-img' />
+                <span>Termina em 2 dias</span>
+            </div>
         </div>
     )
 }
