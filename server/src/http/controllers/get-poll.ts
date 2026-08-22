9342af763717ce56df93f9262a7bb410ee17fcaf
HenriqueHiideki <henrique.hiideki@gmail.com>
2026-08-21 21:38:47 -0300

    Atualiza campos de recepcao de dados

--- server/src/http/controllers/get-poll.ts
+++ server/src/http/controllers/get-poll.ts
@@ -1,31 +1,48 @@
-import { FastifyRequest, FastifyReply } from "fastify"
-import { z } from "zod"
-import { prisma } from "../../lib/prisma"
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../../lib/prisma'
 
-export async function getPoll (request: FastifyRequest, reply: FastifyReply){
-    const getPollParams = z.object({
-        pollId: z.coerce.number(),
-    })
+export async function getPoll(request: FastifyRequest, reply: FastifyReply) {
+  const getPollParams = z.object({
+    pollId: z.coerce.number(),
+  })
 
-    const { pollId } = getPollParams.parse(request.params)
+  const { pollId } = getPollParams.parse(request.params)
 
-    const poll = await prisma.polls.findUnique({
-        where: {
-           id: pollId,
-        },
-        include: {
-            options: {
-                select: {
-                    id: true, 
-                    option_text: true,
-                },
+  const poll = await prisma.polls.findUnique({
+    where: {
+      id: pollId,
+    },
+    include: {
+      options: {
+        select: {
+          id: true,
+          option_text: true,
+          _count: {
+            select: {
+              votes: true,
             },
+          },
         },
-    })
+      },
+    },
+  })
 
-    if (!poll) {
-        return reply.status(404).send({ message: 'Enquete nao encontrada, '})
-    }
+  if (!poll) {
+    return reply.status(404).send({ message: 'Enquete não encontrada.' })
+  }
 
-    return reply.status(200).send({ poll })
+  return reply.status(200).send({
+    poll: {
+      id: poll.id,
+      question: poll.question,
+      created_in: poll.created_in,
+      user_id: poll.user_id,
+      options: poll.options.map((option) => ({
+        id: option.id,
+        option_text: option.option_text,
+        votes: option._count.votes,
+      })),
+    },
+  })
 }
