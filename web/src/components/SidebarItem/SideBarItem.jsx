a160334010cb066e7ff0ed6f1d9ce9db57ebf86c
HenriqueHiideki <henrique.hiideki@gmail.com>
2026-08-31 19:44:29 -0300

    Corrige caminho dos icones

--- web/src/components/SidebarItem/SideBarItem.jsx
+++ web/src/components/SidebarItem/SideBarItem.jsx
@@ -3,24 +3,22 @@ import './sidebaritem-style.css'
 export const SideBarItem = [
     {
         title: "Home",
-        icon: <img src="../public/icon-home.png" alt="icone de home" />,
+        icon: <img src="/icon-home.png" alt="icone de home" />,
         link: "/home"
     },
     {
         title: "Dashboard",
-        icon: <img src="../public/icon-dashboard.png" alt="icone de dashboard" />,
+        icon: <img src="/icon-dashboard.png" alt="icone de dashboard" />,
         link: "/dashboard"
     },
     {
         title: "Vote",
-        icon: <img src="../public/icon-vote.png" alt="icone de vote" />,
+        icon: <img src="/icon-vote.png" alt="icone de vote" />,
         link: "/vote"
     },
     {
         title: "Settings",
-        icon: <img src="../public/icon-settings.png" alt="icone de home" />,
+        icon: <img src="/icon-settings.png" alt="icone de home" />,
         link: "/settings"
     }
 ]
-
-//Home, Dashboard, My Polls, Settings
